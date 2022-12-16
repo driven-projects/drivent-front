6231168cbe6a14fc01bb5446b2414493be6065db
Tukkos <jpfcastro@id.uff.br>
2022-12-16 19:38:55 -0300

    feat: now with or without ticket hotel option to choose

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -8,12 +8,14 @@ import PaymentOptionsBox from '../../../components/Payment/PaymentOptionsBox';
 export default function Payment() {
   const { enrollment } = useEnrollment();
 
-  const [isRemote, setIsRemote] = useState(false);
-  console.log(isRemote);
-  // const [includesHotel, setIncludesHotel] = useState(false);
+  const [isRemote, setIsRemote] = useState();
+  const [includesHotel, setIncludesHotel] = useState();
 
   const [isPresentialActive, setIsPresentialActive] = useState(false);
   const [isRemoteActive, setIsRemoteActive] = useState(false);
+
+  const [isHotelActive, setIsHotelActive] = useState(false);
+  const [isNonHotelActive, setIsNonHotelActive] = useState(false);
 
   function selectPresential() {
     setIsRemote(false);
@@ -30,22 +32,43 @@ export default function Payment() {
       setIsPresentialActive(false);
     }
   }
+
+  function selectWithoutHotel() {
+    setIncludesHotel(false);
+    setIsHotelActive(!isHotelActive);
+    if(isNonHotelActive === true) {
+      setIsNonHotelActive(false);
+    };
+  };
+
+  function selectWithHotel() {
+    setIncludesHotel(true);
+    setIsNonHotelActive(!isNonHotelActive);
+    if(isHotelActive === true) {
+      setIsHotelActive(false);
+    }
+  }
   
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+
       {(!enrollment) ?
+      
         <StyledCenteredText>
           <StyledTypography variant="h6">
             Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso
           </StyledTypography>
         </StyledCenteredText>
+
         :
+
         <>
           <TicketTypeModelBar>
             <StyledTypography variant="h6">
               Primeiro, escolha sua modalidade de ingresso
             </StyledTypography>
+
             <OptionsBar>
               <PaymentOptionsBox onClick={selectPresential} 
                 style={{
@@ -73,7 +96,50 @@ export default function Payment() {
               </PaymentOptionsBox>
             </OptionsBar>
           </TicketTypeModelBar>
-          
+
+          {(isRemote === false) ?
+
+            <TicketTypeModelBar>
+              <StyledTypography variant="h6">
+                Ótimo! Agora escolha sua modalidade de hospedagem
+              </StyledTypography>
+
+              <OptionsBar>
+                <PaymentOptionsBox onClick={selectWithoutHotel} 
+                  style={{
+                    backgroundColor: isHotelActive ? '#FFEED2' : ''
+                  }}
+                >
+                  <h6>
+                    Sem hotel
+                  </h6>
+                  <h6>
+                    + R$ 0
+                  </h6>
+                </PaymentOptionsBox>
+                <PaymentOptionsBox onClick={selectWithHotel}
+                  style={{
+                    backgroundColor: isNonHotelActive ? '#FFEED2' : ''
+                  }}
+                >
+                  <h6>
+                    Com hotel
+                  </h6>
+                  <h6>
+                    + R$ 250
+                  </h6>
+                </PaymentOptionsBox>
+              </OptionsBar>
+            </TicketTypeModelBar>
+
+            :
+
+            <TicketTypeModelBar>
+              <StyledTypography variant="h6">
+                Fechado! O total ficou em R$ 100. Agora é só confirmar:
+              </StyledTypography>
+            </TicketTypeModelBar>
+          }
         </>
       }  
     </>
