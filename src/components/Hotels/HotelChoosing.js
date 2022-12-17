a69d71b9bbbc5c0679b4e5965b9450cfe512880a
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-12-17 14:52:33 -0300

    refactor(hotels): separating components in different files

--- src/components/Hotels/HotelChoosing.js
+++ src/components/Hotels/HotelChoosing.js
@@ -2,6 +2,7 @@ import { Typography } from '@material-ui/core';
 import { useState } from 'react';
 import styled from 'styled-components';
 import useHotels from '../../hooks/api/useHotels';
+import Hotel from './Hotel';
 
 export default function ChooseHotel() {
   const { hotels, hotelsError, hotelsLoading } = useHotels();
@@ -43,33 +44,5 @@ const Hotels = styled.div`
   overflow-x: scroll;
   ::-webkit-scrollbar {
     display: none;
-  }
-`;
-
-function Hotel({ hotel: { id, name, image }, selected: { selected, setSelected } }) {
-  return (
-    <HotelContainer selected={selected===id} onClick={() => setSelected(id)}>
-      <img src={image} alt='' />
-      <h4>{name}</h4>
-    </HotelContainer>
-  );
-}
-
-const HotelContainer = styled.div`
-  width: 196px;
-  height: 264px;
-  background-color: ${props => (props.selected)? '#FFEED2;' : '#EBEBEB;'};
-  border-radius: 10px;
-  margin-right: 19px;
-  padding: 16px 14px 16px 14px;
-  h4 {
-    margin-top: 10px;
-    color: #343434;
-    font-size: 20px;
-  }
-  img {
-    width: 168px;
-    height: 109px;
-    border-radius: 5px;
   }
 `;
